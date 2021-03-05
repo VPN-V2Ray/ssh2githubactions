24ed47424fd5de4fa0008e5b20f953154f51fc52
VPN-V2Ray <46552880+VPN-V2Ray@users.noreply.github.com>
2021-03-06 06:53:43 +0800

    Update tmate2actions.sh

--- tmate2actions.sh
+++ tmate2actions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2020 Tony
+# Copyright (c) 2020-2021 Tony
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
@@ -47,7 +47,7 @@ TMATE_SSH=$(tmate -S ${TMATE_SOCK} display -p '#{tmate_ssh}')
 TMATE_WEB=$(tmate -S ${TMATE_SOCK} display -p '#{tmate_web}')
 MSG="
 * ✈️ GitHub Actions 😊 tmate session info:✈️ *
-✈️=======VPN======⏰ `date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"` ⏱===========✈️
+✈️=====OpenWRT====⏰ `date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"` ⏱===SSH===✈️
 ✳️  *CLI:*   \`${TMATE_SSH}\`
 🌏  *URL:*   ${TMATE_WEB}  🍎
 🔔  *TIPS:*   Run '\`touch ${CONTINUE_FILE}\`' to continue to the next step.
